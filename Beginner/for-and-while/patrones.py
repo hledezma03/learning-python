6e5fb7ee8b3db728dc8e6173c74012dfb04c7e21
Hector <hledezma910@gmail.com>
2025-09-24 11:17:29 -0400

    refactor(patrones): use functions for print the figures

--- Beginner/for-and-while/patrones.py
+++ Beginner/for-and-while/patrones.py
@@ -1,23 +1,35 @@
+def print_square(size):
+    for i in range(size):
+        print('* ' * size)
+
+def print_triangle(size):
+    for i in range(1, size + 1):
+        print(' ' * (size - i) + '* ' * i)
+
+def print_pyramid(size):
+    for i in range(1, size + 1):
+        if i == 1:
+            print(' ' * (size - i) + '*')
+        elif i == size:
+            print('* ' * size)
+        else:
+            print(' ' * (size - i) + '*' + ' ' * (2*i - 3) + '*')
+
+
+
+
 patron = input('Which patron do you want? Square (s), Triangle (t), Piramid (p): ')
 size = int(input('How many * do you want to use? '))
 
 while True:
     if patron == 's':
-        for i in range(size):
-            print('* ' * size)
+        print_square(size)
         break
     elif patron == 't':
-        for i in range(1, size + 1):
-            print(' ' * (size - i) + '* ' * i)
+        print_triangle(size)
         break
     elif patron == 'p':
-        for i in range(1, size + 1):
-            if i == 1:
-                print(' ' * (size - i) + '*')
-            elif i == size:
-                print('* ' * size)
-            else:
-                print(' ' * (size - i) + '*' + ' ' * (2*i - 3) + '*')
+        print_pyramid(size)
         break
     else:
         print('Select a patron. For square enter (s), for triangle enter (s) and for pyramid enter (p): ')
