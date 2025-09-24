3d1a83994f66cf15ec3292b7a944c4cf39ca1954
Hector <hledezma910@gmail.com>
2025-09-24 11:05:27 -0400

    Add convert celsius to fahrenheit and vice versa program

---
+++ Beginner/functions/area-of-figures.py
@@ -0,0 +1,8 @@
+def area_of_circle(radio):
+    return 3.14 * radio * radio
+
+def area_of_square(length):
+    return length * length
+
+def area_of_triangle(base, height):
+    return base * height / 2
---
+++ Beginner/functions/convert.py
@@ -0,0 +1,6 @@
+# Convert fahrenheit to celsius
+def fahr_cel(F):
+    return (F - 32) * 5/9
+
+def cel_fahr(C):
+    return C * 9/5 + 32
